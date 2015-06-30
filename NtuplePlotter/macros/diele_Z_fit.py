5f76462497de2dd9a90e7bf97fc2a91df596ca97
Danny Noonan <dnoonan@cern.ch>
2015-06-30 09:52:20 -0500

    Changed input and output directories

--- NtuplePlotter/macros/diele_Z_fit.py
+++ NtuplePlotter/macros/diele_Z_fit.py
@@ -77,7 +77,10 @@ def getInt_Err(hist):
 # make templates for fitting:
 # Vgamma and ZJets as "Z to ee"
 # everything else as "other"
-fileName = 'templates_presel.root'
+#fileDir = 'di_ele_cross_check_zeroB/'
+
+fileDir = 'di_ele_cross_check/'
+fileName = fileDir+'plots/templates_presel.root'
 hsuffix = '_ele1ele2Mass'
 
 electronTempl = get1DHist(fileName, 'ZJets'+hsuffix)
@@ -86,7 +89,7 @@ electronTempl.Add( get1DHist(fileName, 'Vgamma'+hsuffix) )
 otherTempl = get1DHist(fileName, 'TTJets'+hsuffix)
 otherTempl.Add( get1DHist(fileName, 'TTGamma'+hsuffix) )
 otherTempl.Add( get1DHist(fileName, 'WJets'+hsuffix) )
-otherTempl.Add( get1DHist(fileName, 'Diboson'+hsuffix) )
+#otherTempl.Add( get1DHist(fileName, 'Diboson'+hsuffix) )
 otherTempl.Add( get1DHist(fileName, 'SingleTop'+hsuffix) )
 
 
@@ -94,7 +97,7 @@ dataTempl = get1DHist(fileName, 'Data'+hsuffix)
 
 lowmass = 20.0
 highmass = 180.0
-(eleFrac, eleFracErr) = makeFit('ele1ele2Mass', lowmass, highmass, electronTempl, otherTempl, dataTempl, 'e_e_fit.png')
+(eleFrac, eleFracErr) = makeFit('ele1ele2Mass', lowmass, highmass, electronTempl, otherTempl, dataTempl, fileDir+'e_e_fit.png')
 
 print 'Finding fit range in bins'
 lowbin = dataTempl.FindBin(lowmass + 0.01)
