5b931a3a4c89acc9aec374be1a96df997eed517d
Danny Noonan <dnoonan@cern.ch>
2015-07-13 21:53:06 -0500

    Add more data points at high fractions

--- NtuplePlotter/macros/fit_cross_check.py
+++ NtuplePlotter/macros/fit_cross_check.py
@@ -13,7 +13,7 @@ templateFits.NpseudoExp = 0
 #templateFits.datasetsToMix = ['TTGamma','TTJets']
 #templateFits.datasetsToMix = ['MGTTGamma','TTJets']
 
-scales = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 5.0]
+scales = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.75, 3.0, 3.25, 3.5, 4.0, 4.5,5.0]
 #scales = [1.0]
 graph = ROOT.TGraphErrors(len(scales))
 gpoint = 0
