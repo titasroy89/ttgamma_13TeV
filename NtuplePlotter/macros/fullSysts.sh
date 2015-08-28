9a036d2aa28c9dadc294e2187752be8116807e8e
Danny Noonan <dnoonan@cern.ch>
2015-08-28 09:10:59 -0500

    fix a coulpe issues with running systematics

--- NtuplePlotter/macros/fullSysts.sh
+++ NtuplePlotter/macros/fullSysts.sh
@@ -2,15 +2,17 @@
 #!/bin/bash
 echo "Running all"
 
-channel="ele"
-outDir="ratioFiles/"
+channel="mu"
+outDir="ratioFiles_${channel}/"
 
 mkdir ${outDir}
 
-Systematics=("Btag_up" \
+Systematics=("MuEff_up" \
+"MuEff_down" \
+"musmear_up" \
+"musmear_down" \
+"Btag_up" \
 "Btag_down" \
-"EleEff_up" \
-"EleEff_down" \
 "EleFakeSF_up" \
 "EleFakeSF_down" \
 "JEC_up" \
@@ -25,25 +27,33 @@ Systematics=("Btag_up" \
 "otherMC_down" \
 "ZJetsSF_up" \
 "ZJetsSF_down" \
-"elesmear_up" \
-"elesmear_down" \
 "pho_up" \
 "pho_down" \
 "toppt_up" \
 "toppt_down")
 
+# if [ ${channel}=="mu" ]; then
+#     Systematics[0]="MuEff_up"
+#     Systematics[1]="MuEff_down"
+#     Systematics[2]="musmear_up"
+#     Systematics[3]="musmear_down"
+# fi
+
 for i in `seq 0 23`; do
-    echo "python makePlots.py ${channel} ${Systematics[i]}"
-    python makePlots.py ${channel} ${Systematics[i]}
+    echo "python makePlots.py ${channel} ${Systematics[i]} SaveOutput"
+    python makePlots.py ${channel} ${Systematics[i]} SaveOutput
     echo "cp templates_barrel_scaled_afterPhotonM3.root ${outDir}templates_barrel_scaled_afterPhotonM3_${Systematics[i]}.root"
     cp templates_barrel_scaled_afterPhotonM3.root ${outDir}templates_barrel_scaled_afterPhotonM3_${Systematics[i]}.root
     cp templates_barrel_scaled.root ${outDir}templates_barrel_scaled_${Systematics[i]}.root
 done
 
-echo "python makePlots.py ${channel} > ${outDir}ratio_nominal.txt"
-python makePlots.py ${channel} > ${outDir}ratio_nominal.txt
+echo "python makePlots.py ${channel} SaveOutput"
+python makePlots.py ${channel} SaveOutput
 
 echo "cp templates_barrel_scaled_afterPhotonM3.root ${outDir}templates_barrel_scaled_afterPhotonM3_nominal.root"
 cp templates_barrel_scaled_afterPhotonM3.root ${outDir}templates_barrel_scaled_afterPhotonM3_nominal.root
 cp templates_barrel_scaled.root ${outDir}templates_barrel_scaled_nominal.root
 
+cp TTGamma_SF_Lkhood.png TTGamma_SF_Lkhood_${channel}.png
+cp Vgamma_SF_Lkhood.png Vgamma_SF_Lkhood_${channel}.png
+cp jet_gamma_SF_Lkhood.png jet_gamma_SF_Lkhood_${channel}.png
