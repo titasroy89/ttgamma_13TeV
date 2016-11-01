bdd5b72b316a3fdd4b8bd20e4ed996f2d148d2d6
titasroy89 <titasroy89@users.noreply.github.com>
2016-11-01 12:39:10 -0500

    working node

--- run_skimmer.sh
+++ run_skimmer.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 job=$1
-#job=5
 
-cd /uscms/home/troy2012/ttgamma_13TeV/CMSSW_7_4_14/src/
+workingnode=$(pwd)
+
+echo $workingnode
+
+tar -xzf Summer15_25nsV6_MC.tar.gz
+
+cd /uscms_data/d3/troy2012/ttgamma_13TeV/CMSSW_7_4_14/src/TTGammaSemiLep/
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-eval `scram runtime -sh`
-cd /uscms/home/troy2012/ttgamma_13TeV/CMSSW_7_4_14/src/TTGammaSemiLep/
+eval `scramv1 runtime -sh`
 
-outputDir="/uscms/home/troy2012/ttgamma_13TeV/CMSSW_7_4_14/src/TTGammaSemiLep/"
-inputfiles=("sync.root /uscms/home/troy2012/ttgamma_13TeV/CMSSW_7_4_14/src/ggAnalysis/ggNtuplizer/test/ggtree_sync.root")
+cd $workingnode
+outputdir="root://cmseos.fnal.gov//store/user/troy2012/skims_13TeV/"
+outputfiles=("data_d_PR.root" \
+"st_t.root")
+inputfiles=("root://cmseos.fnal.gov//store/user/troy2012/ntuples_13TeV/job_SingleMu_Run2015D_PR_v4_miniAOD.root" \
+"root://cmseos.fnal.gov//store/user/troy2012/ntuples_13TeV/job_spring15_ST_t_top_4f_miniAOD.root")
+echo "./makeSkim ${outputfiles[job]} ${inputfiles[job]}"
+./makeSkim ${outputfiles[job]} ${inputfiles[job]}
 
-echo "NtuplePlotter/makeSkim ${outputDir}${inputfiles[job]}"
+echo " xrdcp -f ${outputfiles[job]} ${outputdir} "
+xrdcp -f ${outputfiles[job]} ${outputdir}
 
-NtuplePlotter/makeSkim ${outputDir}${inputfiles[job]}
